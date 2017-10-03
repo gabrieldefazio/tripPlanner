41856845646065330080af52a6ada7ef0bddf316
Anna Medyukh <annamedyukh@gmail.com>
2017-10-03 12:39:48 -0400

    added build marker function, at Marker Factory Module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 console.log("we're so cool");
 
 const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+const marker = buildMarker("activity", [-74.009151, 40.705086]);
 
-mapboxgl.accessToken = "YOUR API TOKEN HERE";  //we don't know yet??
+mapboxgl.accessToken = "pk.eyJ1IjoibWlzc2FuaWNoa2EiLCJhIjoiY2o4YnNpODV1MDFhczJ3cG0wNWRpcmtpdyJ9.vdrU_rE5GS8Gh5mMdH1MhQ";
 
 const map = new mapboxgl.Map({ 
   container: "map",
@@ -10,3 +12,10 @@ const map = new mapboxgl.Map({
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+marker.addTo(map);
