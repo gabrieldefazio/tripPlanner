4830604daceed7b9c853ead261257b97d3c20a92
maiya <littlejuniebug@gmail.com>
2017-10-03 11:55:56 -0400

    At step Maapbox MPN Module.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("we're so cool");
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "YOUR API TOKEN HERE";  //we don't know yet??
+
+const map = new mapboxgl.Map({ 
+  container: "map",
+  center: [-74.009, 40.705], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
